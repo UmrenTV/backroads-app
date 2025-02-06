5ca93484ed929f7b2f894584e693630404d95839
Jane Zdravevski <jane@zdravevski.pro>
2025-02-06 09:56:40 +0100

    refactored the components to remove the duplicate code and use iteration instead; added data.js for links management

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import logo from "../images/logo.svg";
+import { pageLinks, socialLinks } from "../data";
 
 function Navbar() {
     return (
@@ -15,59 +16,23 @@ function Navbar() {
                     </button>
                 </div>
                 <ul className="nav-links" id="nav-links">
-                    <li>
-                        <a href="#home" className="nav-link">
-                            home
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#about" className="nav-link">
-                            about{" "}
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#services" className="nav-link">
-                            services
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#tours" className="nav-link">
-                            tours
-                        </a>
-                    </li>
+                    {pageLinks.map(({ id, href, text }) => (
+                        <li key={id}>
+                            <a href={href} className="nav-link">
+                                {text}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
 
                 <ul className="nav-icons">
-                    <li>
-                        <a
-                            href="https://www.twitter.com"
-                            target="_blank"
-                            className="nav-icon"
-                        >
-                            <i className="fab fa-facebook"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.twitter.com"
-                            target="_blank"
-                            className="nav-icon"
-                        >
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.twitter.com"
-                            target="_blank"
-                            className="nav-icon"
-                        >
-                            <i className="fab fa-squarespace"></i>
-                        </a>
-                    </li>
+                    {socialLinks.map(({ id, href, icon }) => (
+                        <li key={id}>
+                            <a href={href} target="_blank" className="nav-icon">
+                                <i className={icon}></i>
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </nav>
